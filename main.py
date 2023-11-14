14b30f8ba44d3c5eff539790a46eec3718a776d4
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-11-14 22:49:02 +0900

    add host

--- main.py
+++ main.py
@@ -52,4 +52,4 @@ async def food_image(image_id: int):
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', port=8000, reload=True)
+    uvicorn.run('main:app', port=8000, host='0.0.0.0', reload=True)
